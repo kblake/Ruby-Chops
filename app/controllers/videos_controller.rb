f9616c2e709eb36b10bc6d2f3b6c22d61aa8c76e
Karmen Blake <karmenblake@gmail.com>
2009-05-16 21:10:09 -0700

    refactored model and added views

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,11 +1,5 @@
 class VideosController < ApplicationController
   def index
-    viddler = ViddlerVideo.new
-    videos = viddler.videos_for("kblake")
-    
-    #TODO: put this stuff into view
-    output_array = videos.map{|v| v.title}
-    output = output_array.join("<br />")
-    render :text=>output
+    @videos = Video.videos_for("kblake")
   end
 end
